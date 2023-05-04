cc3b7fe3e31f15ea212a05278e493591a19b2c17
Gomathi Shankar <gomathishankar37@gmail.com>
2023-05-04 09:57:56 +0000

    Refactor: Linting, Update README

--- systemd-analyze-plot-parser.py
+++ systemd-analyze-plot-parser.py
@@ -2,6 +2,7 @@ import sys
 import mysql.connector
 from svgelements import *
 
+
 def time_stamp_validation(timeVal):
     """
     Validate and Convert the parse svg Time into seconds
@@ -16,9 +17,9 @@ def time_stamp_validation(timeVal):
     timeVal: string
         In-place time converted string
     """
-    if (timeVal[-2:] == "ms"):
+    if timeVal[-2:] == "ms":
         timeVal = str(float(timeVal[0:-2]) / 1000)
-    elif (timeVal[-1:] == "s"):
+    elif timeVal[-1:] == "s":
         timeVal = str(float(timeVal[0:-1]))
     return timeVal
 
@@ -56,7 +57,8 @@ def insert_row(row_list, table_name):
         Name of the Table to be inserted
     """
     row_tuple = tuple(row_list)
-    insert_query = '''INSERT INTO ''' + table_name + '''(service_name, activation_time, start_time, category) VALUES (%s, NULLIF(%s, "NULL"), %s, %s)'''
+    insert_query = '''INSERT INTO ''' + table_name + \
+        '''(service_name, activation_time, start_time, category) VALUES (%s, NULLIF(%s, "NULL"), %s, %s)'''
     DB_cur.execute(insert_query, row_tuple)
     conn.commit()
 
@@ -91,7 +93,7 @@ def parse_SVG(dir_val):
     dir_val: string
         directory name that has the systemd-analyze plot SVG
     """
-    svg = SVG.parse(dir_val + "/systemdanalyze_plot_"+ dir_val +".svg")
+    svg = SVG.parse(dir_val + "/systemdanalyze_plot_" + dir_val + ".svg")
     svg_list.append(svg)
 
 
@@ -99,7 +101,8 @@ svg_list = []
 
 
 # SVG Parsing ignore list
-svg_ignore_strings = ["Linux", "Startup", "Activating", "Active", "Deactivating", "Setting", "Generators", "Loading"]
+svg_ignore_strings = ["Linux", "Startup", "Activating",
+                      "Active", "Deactivating", "Setting", "Generators", "Loading"]
 
 
 # Hard-coded DB Name
@@ -108,9 +111,9 @@ dbName = "systemd_analyze_plotter"
 
 # MySQL DB Connection
 conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="admin",
+    host="localhost",
+    user="root",
+    password="admin",
 )
 
 
@@ -124,22 +127,22 @@ DB_cur.execute(use_DB_query)
 
 
 # directory list
-directories = [] # ["dir1/", "dir2/", "dir3/", ...]
+directories = []  # ["dir1/", "dir2/", "dir3/", ...]
 for i in sys.argv:
     if not i in directories:
         if i[-1] == "/":
             directories.append(i[:-1])
         else:
             directories.append(i)
-directories.pop(0) # remove executable from the list
+directories.pop(0)  # remove executable from the list
 
 
 table_list = []
-for dir in directories:
-    table_list.append(dir)
+for dir_iter in directories:
+    table_list.append(dir_iter)
     # create tables for each dir
-    create_table(dir)
-    parse_SVG(dir)
+    create_table(dir_iter)
+    parse_SVG(dir_iter)
 
 
 list_len = len(svg_list)
@@ -152,15 +155,18 @@ for i in range(list_len):
         if isinstance(element, SVGText):
             mini_list = []
             first_half = element.text.split()
-            if(first_half[0] not in svg_ignore_strings) and (not is_float(first_half[0][:-1])):
+            if (first_half[0] not in svg_ignore_strings) and (not is_float(first_half[0][:-1])):
                 if len(first_half) == 2:
-                    first_half[-1] = time_stamp_validation(first_half[-1][1:-1]) # (3.456s)
+                    # (3.456s)
+                    first_half[-1] = time_stamp_validation(
+                        first_half[-1][1:-1])
                     mini_list.extend(first_half)
-                elif len(first_half) == 3: # [cec.service, (1min, 16.3s)]
-                    seconds_section = float(time_stamp_validation(first_half[-1][:-1])) #16.3
+                elif len(first_half) == 3:  # [cec.service, (1min, 16.3s)]
+                    seconds_section = float(
+                        time_stamp_validation(first_half[-1][:-1]))  # 16.3
                     minutes_section = float(first_half[-2][1:-3]) * 60
                     first_half.pop(-1)
-                    first_half[1] =str(minutes_section + seconds_section)
+                    first_half[1] = str(minutes_section + seconds_section)
                     mini_list.extend(first_half)
                     pass
                 else:
